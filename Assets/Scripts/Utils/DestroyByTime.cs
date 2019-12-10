a444b3ec00f280ddeb9d4f8c69a99c6729f09e82
Sergii Momot <31519774+SOMStudio@users.noreply.github.com>
2019-12-10 20:11:45 +0200

    DestroyByTime - destroy particle object after predefine time

---
+++ Assets/Scripts/Utils/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+	[SerializeField]
+	private float destroyTime;
+
+	// main events
+	void Start () {
+		Destroy (gameObject, destroyTime);
+	}
+
+}
